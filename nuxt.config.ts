9503653d7907ee0f2efdf247e2dd24f0313a4f15
Yuriy Bakutin <yuriybakutin@gmail.com>
2025-03-09 12:31:18 +0300

    The vuetify-nuxt-module is installed and configured

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+
   devServer: {
     port: process.env.NITRO_PORT ? +process.env.NITRO_PORT : 4012,
   },
+
+  modules: ['vuetify-nuxt-module'],
 })
